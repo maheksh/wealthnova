8c58dbe5b70af18de925131161fc06a3f721b28a
maheksh <email-of-maheksh>
2025-10-03 19:48:56 +0530

    Extend layout to full screen with 100% width and fix #666 border on input-bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-content">
-        <h1 className="navbar-title">Wize Wealth</h1>
+        <h1 className="navbar-title">WealthNova</h1>
       </div>
     </nav>
   );
